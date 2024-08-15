c42d6f94924a24006e8f8ef89cfe82f3a0ba97ff
requisz <prk3479@gmail.com>
2024-08-14 23:55:49 -0400

    starting Amazon Cognito Authentication

---
+++ aws-exports.js
@@ -0,0 +1,10 @@
+// src/aws-exports.js
+const awsmobile = {
+    "aws_project_region": "us-east-2",
+    "aws_cognito_region": "us-east-2",
+    "aws_user_pools_id": "us-east-2_TkgqubIgu",
+    "aws_user_pools_web_client_id": "2kc2qph2rhiibdj708l07jfvcq",
+    "oauth": {}
+};
+
+export default awsmobile;
